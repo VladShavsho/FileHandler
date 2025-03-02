d04609ca75d2efd0a94f971b8c892c33184701a2
VladislavSCH <114906748+VladislavSCH@users.noreply.github.com>
2025-03-02 22:25:36 +0200

    Program update File Handler

--- src/app/FileHandler.java
+++ src/app/FileHandler.java
@@ -5,10 +5,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileHandler {
-    private final static String BASE_PATH_OUT = "/Hill/Java/DEMO/ABC_16/FileHandler/";
+    private final static String BASE_PATH = "/Hill/Java/DEMO/ABC_16/FileHandler/";
 
     public String writeFile(String fileName, String fileContent) {
-        try (FileWriter fw = new FileWriter(BASE_PATH_OUT + fileName + ".txt")) {
+        try (FileWriter fw = new FileWriter(BASE_PATH + fileName + ".txt")) {
             fw.write(fileContent);
             return "Success.";
         } catch (Exception e) {
@@ -17,7 +17,7 @@ public class FileHandler {
     }
 
     public String readFile(String fileName) {
-        try (FileReader reader = new FileReader(BASE_PATH_OUT + fileName + ".txt")) {
+        try (FileReader reader = new FileReader(BASE_PATH + fileName + ".txt")) {
             int sym;
             StringBuilder stringBuilder = new StringBuilder();
             while ((sym = reader.read()) != -1) {
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,13 @@
 package app;
 
 public class Main {
-    private  final static String BASE_PATH = "/Hill/Java/DEMO/ABC_16/FileHandler/";
+
     public static void main(String[] args) {
         FileHandler handler = new FileHandler();
         String fileName = "myfile";
         String fileContent = "My very important information.";
         String result = handler.writeFile(fileName, fileContent);
-        String content = handler.readFile(BASE_PATH + fileName + ".txt");
+        String content = handler.readFile(fileName);
         getOutput("RESULT: " + result);
         getOutput("FILE CONTENT: " + content);
     }
